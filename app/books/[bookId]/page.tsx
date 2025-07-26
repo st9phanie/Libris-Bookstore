4f77bc0c28e6504562a3347105fe5a54568b168d
steph <stephguiragossian@gmail.com>
2025-07-26 15:44:20 +0300

    test

--- app/books/[bookId]/page.tsx
+++ app/books/[bookId]/page.tsx
@@ -1,15 +1,13 @@
 import { DisplayBookInfo } from '@/components/book-page/displayBookInfo';
 import { getBookById } from "@/lib/getBookById";
 
-interface BookPageProps {
-  params: {
-    bookId: string; // plain string, no Promise
-  };
-}
-
-export default async function BookInfo({ params }: BookPageProps) {
-  const bookid = Number(params.bookId);
-  const book = await getBookById(bookid);
+export default async function BookInfo({
+  params,
+}: {
+  params: { bookId: string };
+}) {
+  const bookId = Number(params.bookId);
+  const book = await getBookById(bookId);
 
   return (
     <div>
