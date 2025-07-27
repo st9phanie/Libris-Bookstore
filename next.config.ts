a8f2faba2b21d456d4b509941dc34c09b5bfac47
steph <stephguiragossian@gmail.com>
2025-07-27 11:36:57 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["beautifulbooks.info","ocjplxadxfrfujzwdmyy.supabase.co"], 
+    domains: ["beautifulbooks.info","ocjplxadxfrfujzwdmyy.supabase.co","libris-bookstore.vercel.app"], 
   },
 };
 
